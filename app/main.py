b94f1ba3d070ccaa6a2564cc55a0ecf0e408c7bc
Av3roRR <andrey_emelyanov_2005@mail.ru>
2024-08-25 00:11:59 +0300

    Разобрался с подключением к базе данных и подключил роутеры и сделал migration бд

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from app.notes.router import router as router_notes
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+app.include_router(router_notes)
--- app/notes/router.py
+++ app/notes/router.py
@@ -9,7 +9,7 @@ router = APIRouter(
 
 
 # получаем все заметки из базы данных и в будущем отображаем их на страничке
-router.get("")
+@router.get("")
 async def get_notes():
     notes = await NotesDAO.find_all()
 
@@ -23,8 +23,10 @@ find_all(user_id=user.id, id=note_id)
 а входные данные
 async def get_note_by_id(note_id: int, user=Depends(get_current_user))
 """
-router.get("/{note_id}")
+@router.get("/{note_id}")
 async def get_note_by_id(note_id: int):
     note = await NotesDAO.find_one_or_none(id=note_id)
     if note is None:
         return "Ошибочка вышла"
+    
+    return note
